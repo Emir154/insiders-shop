c884ab35c81aa14fb7768448e714f560cf847546
Emir <example@gmail.com>
2026-01-09 11:40:40 +0200

    added navBar 1-gen

---
+++ src/app/utils/navTabs/navTabs.jsx
@@ -0,0 +1,82 @@
+export const createNavTabs = () => {
+  return [
+    {
+      name: "Dashboard",
+      icon: "/icons/01-align-center.svg",
+      draggable: true,
+      url: "/",
+    },
+    {
+      name: "Banking",
+      icon: "/icons/fi-rs-bank.svg",
+      draggable: true,
+      url: "/Banking",
+    },
+    {
+      name: "Telephone",
+      icon: "/icons/fi-rs-book-alt.svg",
+      draggable: true,
+      url: "/Telephone",
+    },
+    {
+      name: "Account",
+      icon: "/icons/fi-rs-browser.svg",
+      draggable: true,
+      url: "/Account",
+    },
+    {
+      name: "Sale",
+      icon: "/icons/fi-rs-shop.svg",
+      draggable: true,
+      url: "/Sale",
+    },
+    {
+      name: "Statistics",
+      icon: "/icons/fi-rs-chart-pie.svg",
+      draggable: true,
+      url: "/Statistics",
+    },
+    {
+      name: "Post Office",
+      icon: "/icons/fi-rs-bank-11-06-29.svg",
+      draggable: true,
+      url: "/PostOffice",
+    },
+    {
+      name: "Administration",
+      icon: "/icons/fi-rs-settings.svg",
+      draggable: true,
+      url: "/Administration",
+    },
+    {
+      name: "Help",
+      icon: "/icons/fi-rs-book-alt.svg",
+      draggable: true,
+      url: "/Help",
+    },
+    {
+      name: "Inventory",
+      icon: "/icons/fi-rs-cube.svg",
+      draggable: true,
+      url: "/Inventory",
+    },
+    {
+      name: "Selection lists",
+      icon: "/icons/fi-rs-list.svg",
+      draggable: true,
+      url: "/SelectionLists",
+    },
+    {
+      name: "Shopping",
+      icon: "/icons/fi-rs-shopping-cart-check.svg",
+      draggable: true,
+      url: "/Shopping",
+    },
+    {
+      name: "Calc",
+      icon: "/icons/fi-rs-browser.svg",
+      draggable: true,
+      url: "/Calc",
+    },
+  ];
+};
